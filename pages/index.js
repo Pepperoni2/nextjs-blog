8e86ede5b811459541e7b4c71409a938769e9679
Florian Golubic <54410466+Pepperoni2@users.noreply.github.com>
2021-09-20 14:16:29 +0200

    bruh

--- pages/index.js
+++ pages/index.js
@@ -5,46 +5,82 @@ import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
 
-export async function getStaticProps(){
-  const allPostsData = getSortedPostsData()
-  return{
-    props: {
-      allPostsData
-    }
-  }
+export default function Home(){
+  
+<!DOCTYPE html>
+<html lang="en">
+<Head> 
+  <title>Eventx</title>
+</Head>
+
+<body>
+    <div id="wrapper">
+
+        <nav id="nav">
+        <div id="wrpnav">
+            <div id="wrph1">
+                <div id="wrplh">
+                    <div id="logo"></div>
+                    <h1><a href="#">Event-Manager</a></h1>
+                </div>
+                
+                <span class="h1aspan"></span>
+            </div>
+        
+            <div id="wrpbt">
+                
+               <div id="Popup">
+                <div id="flexPop">
+                <button class="bt" >Home</button>
+                <button class="bt" >Impressum</button>
+                <button class="bt" >Login</button>
+                 </div>
+               </div>  
+               
+ 
+            </div>
+            
+        </div>
+       
+        
+        </nav>
+        
+        <main id="main">
+            <div id="mainbackgr"></div>
+            <div id="p-div">
+                
+                <p id="p1">
+
+                Vielseitige <i>Eventplannung!</i>
+                
+                </p>
+               
+                <button onclick="">Mehr erfahren</button>
+            </div>
+           
+        </main>
+        <p id="p2">
+            Einfach und sicher Veranstaltungen plannen und teilen!
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est quis facere error unde, ipsa totam omnis, corrupti a tenetur aspernatur natus delectus, rem voluptatum odit? Aspernatur dignissimos facere ut voluptates.               
+        </p>
+        <footer id="footer">
+        <div id="wrpfooter">
+            <div id="logo1"></div>
+                <li>
+                    Copyright 2021
+                </li>
+                
+                <li>
+                    <a href="#">Impressum</a>
+                </li>
+                
+                    
+                
+            
+            
+        </div>
+        </footer>
+    </div>
+</body>
 }
-
-export default function Home({allPostsData}){
-  return(
-    <Layout home>
-        <Head>
-            <title>{siteTitle}</title>
-        </Head>
-        <section className={utilStyles.headingMd}>
-            <p>[Your self Instruction]</p>
-            <p>
-              (This is a sample website - you’ll be building a site like this on{' '}
-              <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-            </p>
-        </section>
-        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({id, date, title}) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date}/>
-              </small>
-            </li>
-          ))}
-        </ul>
-
-        </section>
-    </Layout>
-
-  )
-}
+            
