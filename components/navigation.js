9376601d45a8c1a6809901a4c1a88186ad4189d7
Florian <florian.golubic@gmail.com>
2021-11-02 21:32:21 +0100

    Next-Auth for GitHub
    Registrieren funktioniert mit Github.
    Die Daten werden auf die MongoDB übertragen.

--- components/navigation.js
+++ components/navigation.js
@@ -1,53 +1,53 @@
 import Link from "next/dist/client/link";
-
+import { signIn, signOut, useSession} from 'next-auth/client'
 
 function Nav(props) {
-/*
-    const mq = typeof window.matchMedia( "(max-width: 690px)" );
-
-  var prevScrollpos = window.pageYOffset;
+  /*
+      const mq = typeof window.matchMedia( "(max-width: 690px)" );
   
-  window.onscroll = function() {scrollFunction()};
-  
-  function scrollFunction() {
-    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
-      document.getElementById("nav").style.height="15vh";
-      document.getElementById("nav").style.backgroundColor="rgba(47,47,47, 0.5)";
-      
-    } else {
-      document.getElementById("nav").style.height="20vh"; 
-      document.getElementById("nav").style.backgroundColor="rgba(47,47,47, 1)";  
-    }
-  
-  
-    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150){
-      document.getElementById("wrapper").style.backgroundColor="rgba(47,47,47, 0.5)";
-  
-    }
-    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400){
-      document.getElementById("wrppic").style.backgroundColor="rgba(233, 114, 49, 1)";
-  
-    }
-    else
-    {
-      document.getElementById("wrppic").style.backgroundColor="rgba(47,47,47, 0.5)";
-  
-    }
+    var prevScrollpos = window.pageYOffset;
     
-  } 
-  function scrollWin() {
-    window.scrollBy(0, 300);
-  }
-*/
+    window.onscroll = function() {scrollFunction()};
+    
+    function scrollFunction() {
+      if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+        document.getElementById("nav").style.height="15vh";
+        document.getElementById("nav").style.backgroundColor="rgba(47,47,47, 0.5)";
+        
+      } else {
+        document.getElementById("nav").style.height="20vh"; 
+        document.getElementById("nav").style.backgroundColor="rgba(47,47,47, 1)";  
+      }
+    
+    
+      if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150){
+        document.getElementById("wrapper").style.backgroundColor="rgba(47,47,47, 0.5)";
+    
+      }
+      if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400){
+        document.getElementById("wrppic").style.backgroundColor="rgba(233, 114, 49, 1)";
+    
+      }
+      else
+      {
+        document.getElementById("wrppic").style.backgroundColor="rgba(47,47,47, 0.5)";
+    
+      }
+      
+    } 
+    function scrollWin() {
+      window.scrollBy(0, 300);
+    }
+  */
 
-
+  const [session, loading] = useSession()
   return (
     <nav id="nav">
       <div id="wrpnav">
         <div id="wrph1">
           <div id="wrplh">
             <Link href="/"><div id="logo"></div></Link>
-            
+
             <Link href="/"><a>EventX</a></Link>
           </div>
         </div>
@@ -57,16 +57,38 @@ function Nav(props) {
             <div id="flexPop">
               <button className="bt">Home</button>
               <Link href="/">
-                <button className="bt" onClick="#">
+                <button className="bt">
                   About
                 </button>
               </Link>
 
-              <Link href="/signin">
-                <button className="bt">
-                  <a>Login</a>
-                </button>
-              </Link>
+
+              {!loading && !session && (
+                <Link href='/api/auth/signin'>
+                  <button className="bt">
+                    <a
+                      onClick={e => {
+                        e.preventDefault()
+                        signIn('github','google')
+                      }}>
+                      Login
+                    </a>
+                  </button>
+                </Link>
+              )}
+              {session && (
+                <Link href='/api/auth/signout'>
+                  <button className="bt">
+                    <a
+                      onClick={e => {
+                        e.preventDefault()
+                        signOut()
+                      }}>
+                      Sign Out
+                    </a>
+                  </button>
+                </Link>
+              )}
             </div>
           </div>
         </div>
@@ -76,3 +98,11 @@ function Nav(props) {
 }
 
 export default Nav;
+
+/* -------------- DELETED CODE ------------
+
+              <Link href="/signin">
+                <button className="bt">
+                  <a>Login</a>
+                </button>
+              </Link> */
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,27 +1,19 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
 
+
 export default NextAuth({
     providers: [
         Providers.GitHub({
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET,
         }),
-        Providers.Email({
-            server: {
-              host: process.env.EMAIL_SERVER_HOST,
-              port: process.env.EMAIL_SERVER_PORT,
-              auth: {
-                user: process.env.EMAIL_SERVER_USER,
-                pass: process.env.EMAIL_SERVER_PASSWORD,
-              },
-            },
-            from: process.env.EMAIL_FROM,
-          }),
+        Providers.Google({
+          clientId: process.env.GOOGLE_CLIENT_ID,
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+          authorizationUrl: 'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code',
+        }),
     ],
-    pages: {
-        signIn: "/signin",
-      },
     database: process.env.DB_URL,
     session: {
         jwt: true
