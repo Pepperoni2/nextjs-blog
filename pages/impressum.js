083b0a181702e64184a4fa9dc78d2aa90c8b1105
Zebtagon <daniel.jahoda@hotmail.com>
2022-01-20 16:02:57 +0100

    new

--- pages/impressum.js
+++ pages/impressum.js
@@ -1,12 +1,25 @@
+import { Link } from "react-router-dom";
+import Footer from "../components/footer";
 import styles from "../styles/modules/impressum.module.scss";
 
 export default function Impressum() {
   return (
     <div className={styles.wrapper}>
-      <p>
-        This is a thesis project, please do not pursue any legal action. <br></br>The
-        website is non-commercial and for educational purposes only.
-      </p>
+      <div className={styles.wrap}>
+        <h1>Information</h1>
+        <p>
+          This is a thesis project, please do not pursue any legal action.{" "}
+          <br></br>The website is non-commercial and for educational purposes
+          only.
+        </p>
+
+        <a href="/" className={styles.back}>
+          Go back
+        </a>
+      </div>
+      <div className={styles.footer}>
+        <Footer></Footer>
+      </div>
     </div>
   );
 }
