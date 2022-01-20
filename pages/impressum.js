2816d0e1899d9698e9ba64e718399f0c3728020c
Zebtagon <daniel.jahoda@hotmail.com>
2022-01-20 21:58:22 +0100

    new

--- pages/impressum.js
+++ pages/impressum.js
@@ -7,19 +7,21 @@ export default function Impressum() {
     <div className={styles.wrapper}>
      
       <div className={styles.wrap}>
+        <div className={styles.bwrap}>
         <h1>Information</h1>
         <p>
           This is a thesis project, please do not pursue any legal action.{" "}
           <br></br>The website is non-commercial and for educational purposes
           only.
         </p>
-
+        </div>
         <a href="/" className={styles.back}>
           Go back
         </a>
       </div>
       
       <div className={styles.footer}>
+   
         <Footer></Footer>
       </div>
       
