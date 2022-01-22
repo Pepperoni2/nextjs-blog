1ef37af45274743363f9482812980145381791e4
Donkey Kong <56480086+Zebtagon@users.noreply.github.com>
2022-01-23 00:02:02 +0100

    drag feature + changed distributor

--- pages/login/distributor/index.js
+++ pages/login/distributor/index.js
@@ -5,7 +5,7 @@ import NavLogin from "../../../components/header-login";
 import Header1 from "/components/head";
 import { GiAries } from "@react-icons/all-files/gi/GiAries";
 import { GiAzulFlake } from "@react-icons/all-files/gi/GiAzulFlake";
-import { motion } from "framer-motion";
+import { motion, useAnimation } from "framer-motion";
 import { get } from "jquery";
 import React, { useState, useEffect } from "react";
 import Loading from "../../../components/Loading";
@@ -14,27 +14,55 @@ import { id } from "date-fns/locale";
 
 export default function Distributor(params) {
   const style = { color: "white", fontSize: "1.5em" };
+  const slidess = useAnimation();
+  const controls = useAnimation();
 
-  const [stateText, setText] = useState();
-
-  const changeText = (vari) => {
-    setText(vari);
+  const ani = {
+    hidden: {
+      scale: 1,
+      opacity: 0,
+      x: -200,
+    },
+    visible: {
+      x: 0,
+      scale: 1,
+      opacity: 1,
+      transition: {
+        delay: 1,
+      },
+    },
+  };
+  const slide = {
+    hidden: {
+      opacity: 0,
+      x: 200,
+    },
+    visible: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        delay: 0.2,
+      },
+    },
   };
 
   useEffect(() => {
-    const text = document.getElementById("text");
-    console.log(text);
-    if (text.innerText == "") {
-      text.style.scale = "0";
-    } else {
-      text.style.scale = "1";
-      text.style.opacity = "1";
-      // text.innerHTML = stateText;
-    }
-    
+    controls.set({ opacity: 0, x: 50 });
 
+    controls.start((i) => ({
+      opacity: 1,
+      x: 0,
+      transition: { delay: i * 0.7 },
+    }));
 
-  }, [stateText]);
+    slidess.set({ opacity: 0, x: 50 });
+
+    slidess.start((i) => ({
+      opacity: 1,
+      x: 0,
+      transition: { delay: i * 0.7 },
+    }));
+  }, []);
 
   return (
     <motion.div
@@ -42,14 +70,16 @@ export default function Distributor(params) {
       animate="visible"
       variants={{
         hidden: {
-          scale: 0.9,
+          scale: 1,
           opacity: 0,
+          y: -200,
         },
         visible: {
+          y: 0,
           scale: 1,
           opacity: 1,
           transition: {
-            delay: 0.2,
+            delay: 0.3,
           },
         },
       }}
@@ -57,46 +87,80 @@ export default function Distributor(params) {
       <div className={styles.wrapper}>
         <div className={styles.wrap}>
           <div className={styles.bwrap}>
-            <h1>Choose</h1>
+            <motion.h1 initial={{ pathLength: 0 }} animate={{ pathLength: 1 }}>
+              Choose
+            </motion.h1>
             <div className={styles.flexbwrap}>
-              <span
-                className={styles.chbt}
-                id="0"
-                onClick={() =>
-                  changeText(
-                    "Organizers have the ability to create and to organize your own events or parties"
-                  )
-                }
-              >
-                <h3>Organizer</h3>
-                <GiAries className={styles.icons} />
-                <div className={styles.backg}></div>
-              </span>
+              <Link href="/login/distributor/o-register">
+                <a>
+                  <motion.span
+                    className={styles.chbt}
+                    whileTap={{ scale: 0.95 }}
+                  >
+                    <h3>Organizer</h3>
+                    <GiAries className={styles.icons} />
+                    <div className={styles.backg}></div>
+                  </motion.span>
+                </a>
+              </Link>
 
-              <span
-                className={styles.chbt}
-                id="1"
-                onClick={() =>
-                  changeText(
-                    "Participators have the ability to create and to organize your own events or parties"
-                  )
-                }
-              >
-                <h3>Participator</h3>
-                <GiAzulFlake className={styles.icons} />
-                <div className={styles.backg}></div>
-              </span>
+              <Link href="/login/distributor/p-register">
+                <a>
+                  <motion.span
+                    className={styles.chbt}
+                    whileTap={{ scale: 0.95 }}
+                  >
+                    <h3>Participator</h3>
+                    <GiAzulFlake className={styles.icons} />
+                    <div className={styles.backg}></div>
+                  </motion.span>
+                </a>
+              </Link>
             </div>
             <p className={styles.inptext} id="text">
-              {stateText}
-              {/* <div>Confirm</div> */}
+              <h1>What's the difference?</h1>
+              <motion.div
+                className={styles.textdiv}
+                custom={0}
+                animate={controls}
+              >
+                <h3>Organizers</h3>
+                have the ability to create and to organize your own events or
+                parties.
+              </motion.div>
+
+              <motion.div
+                className={styles.textdiv}
+                custom={1}
+                animate={controls}
+              >
+                <h3>Participators</h3>
+                have the ability to join an event. With that it is possible to
+                see any changes related to the event.
+              </motion.div>
+              <motion.div
+                className={styles.textdiv}
+                custom={2}
+                animate={controls}
+              >
+                <h3>Additional information</h3>
+                Qui excepteur excepteur esse consectetur non. Mollit excepteur
+                id aliqua voluptate aliqua consectetur consectetur. Eu deserunt
+                ipsum culpa Lorem excepteur ad ut aliqua magna ex adipisicing
+                do. Commodo esse consequat nostrud aute sunt qui ut pariatur ad
+                anim ad dolore nostrud. Laborum magna incididunt aute magna duis
+                deserunt ut duis ipsum nisi consectetur laboris sunt.
+              </motion.div>
+              <motion.div className={styles.backtop} whileTap={{ scale: 0.8 }}>
+                &and;
+              </motion.div>
             </p>
           </div>
         </div>
 
-        <div className={styles.footer}>
+        <motion.div className={styles.footer}>
           <Footer></Footer>
-        </div>
+        </motion.div>
       </div>
     </motion.div>
   );
