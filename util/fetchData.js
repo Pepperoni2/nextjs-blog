76f99139aa2138664c855dec4324fd863b8882ab
Florian Golubic <florian.golubic@gmail.com>
2022-01-26 16:39:04 +0100

    changing baseUrl

--- util/fetchData.js
+++ util/fetchData.js
@@ -1,7 +1,7 @@
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
 
 export const getData = async (url, token) => {
-    const res = await fetch(`http://localhost:3000/api/${url}`, {
+    const res = await fetch(`http://eventx-online.com/api/${url}`, {
         method: 'GET',
         headers:{
             'Authorization': token
@@ -41,7 +41,7 @@ export const putData = async (url, post, token) => {
 }
 
 export const patchData = async (url, post, token) => {
-    const res = await fetch(`http://localhost:3000/api/${url}`, {
+    const res = await fetch(`http://eventx-online.com/api/${url}`, {
         method:'PATCH',
         headers:{
             'Content-Type': 'application/json',
