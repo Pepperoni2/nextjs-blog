6ed48816ed35087aebca25ce9d576c0736266c12
Donkey Kong <56480086+Zebtagon@users.noreply.github.com>
2022-02-16 19:50:40 +0100

    nav responsive

--- components/navigationP_after.js
+++ components/navigationP_after.js
@@ -6,9 +6,9 @@ import { GiAbstract027 } from "@react-icons/all-files/gi/GiAbstract027";
 import { MdFavorite } from "@react-icons/all-files/md/MdFavorite";
 import { IoNotificationsSharp } from "@react-icons/all-files/io5/IoNotificationsSharp";
 import { FaSignOutAlt } from "@react-icons/all-files/fa/FaSignOutAlt";
-import React, { useContext, useEffect, useState } from 'react'
-import { DataContext } from '../store/GlobalState'
-import { useRouter } from 'next/router'
+import React, { useContext, useEffect, useState } from "react";
+import { DataContext } from "../store/GlobalState";
+import { useRouter } from "next/router";
 import Cookies from "js-cookie";
 import PopUp from "./popup_after";
 
@@ -20,16 +20,13 @@ export default function NavigationLeftP() {
   //--------------------
   const [isOpen, setIsOpen] = useState(false);
 
-
-  const switchModal = ()=>{
-
+  const switchModal = () => {
     if (isOpen) {
       setIsOpen(false);
-    }
-    else{
+    } else {
       setIsOpen(true);
     }
-  }
+  };
   const isActive = (r) => {
     if (r === router.pathname) {
       return " active";
@@ -38,7 +35,6 @@ export default function NavigationLeftP() {
     }
   };
 
-  
   const logout = () => {
     Cookies.remove("refreshtoken", { path: "api/auth/accessToken" });
     localStorage.removeItem("firstLogin");
@@ -54,24 +50,25 @@ export default function NavigationLeftP() {
           rel="stylesheet"
         />
         <div className={styles.main}>
-        <PopUp open={isOpen} onClose={switchModal}></PopUp>
-          <Link href="/">
-            <div className={styles.divl}>
-              <div className={styles.divlogo}></div>
-            </div>
-          </Link>
-          <div className={styles.divbutton}>
-            <Link href="/participator">
-              <button className={styles.bt1} >
-                <a className={styles.link1 + isActive("/")}>
-                  <h2>
-                    <IoHomeSharp className={styles.icon1} />
-                  </h2>
-                  Events
-                </a>
-              </button>
+          <PopUp open={isOpen} onClose={switchModal}></PopUp>
+          <div className={styles.flexdiv}>
+            <Link href="/">
+              <div className={styles.divl}>
+                <div className={styles.divlogo}></div>
+              </div>
             </Link>
-            <Link href="/participator">
+            <div className={styles.divbutton}>
+              <Link href="/participator">
+                <button className={styles.bt1}>
+                  <a className={styles.link1 + isActive("/")}>
+                    <h2>
+                      <IoHomeSharp className={styles.icon1} />
+                    </h2>
+                    Events
+                  </a>
+                </button>
+              </Link>
+              {/* <Link href="/participator">
               <button className={styles.bt2}>
                 <a className={styles.link2 + isActive("/notifications")}>
                   <h2>
@@ -80,66 +77,72 @@ export default function NavigationLeftP() {
                   Notifications
                 </a>
               </button>
-            </Link>
-            <Link href="/participator">
-              <button className={styles.bt3}>
-                <a className={styles.link3 + isActive("/")}>
-                  <h2>
-                    <MdFavorite className={styles.icon3} />
-                  </h2>
-                  Favourites
-                </a>
-              </button>
-            </Link>
-            <Link href="/yourEvents">
-              <button className={styles.bt4}>
-                <a className={styles.link4 + isActive("/")}>
-                  <h2>
-                    <GiAbstract027 className={styles.icon4} />
-                  </h2>
-                  Your events
-                </a>
-              </button>
-            </Link>
-            <Link href="/settings">
-              <button className={styles.bt5}>
-                <a className={styles.link5 + isActive("/settings")}>
-                  <h2>
-                    <GiAbstract027 className={styles.icon5} />
-                  </h2>{" "}
-                  Settings
-                </a>
-              </button>
-            </Link>
-          </div>
-          <div className={styles.divuser}>
-            <div className={styles.singoutuser}>
-              <Link href="/">
-                <button className={styles.button1} onClick={logout}>
-                  <a className={styles.link6 + isActive("/")}>
-                    <FaSignOutAlt className={styles.icon6} />
-                    Sign out
+            </Link> */}
+              <Link href="/participator">
+                <button className={styles.bt3}>
+                  <a className={styles.link3 + isActive("/")}>
+                    <h2>
+                      <MdFavorite className={styles.icon3} />
+                    </h2>
+                    Favourites
+                  </a>
+                </button>
+              </Link>
+              <Link href="/yourEvents">
+                <button className={styles.bt4}>
+                  <a className={styles.link4 + isActive("/")}>
+                    <h2>
+                      <GiAbstract027 className={styles.icon4} />
+                    </h2>
+                    Your events
+                  </a>
+                </button>
+              </Link>
+              <Link href="/settings">
+                <button className={styles.bt5}>
+                  <a className={styles.link5 + isActive("/settings")}>
+                    <h2>
+                      <GiAbstract027 className={styles.icon5} />
+                    </h2>{" "}
+                    Settings
                   </a>
                 </button>
               </Link>
             </div>
-            <div className={styles.help1}>
-              <div className={styles.logouser} onClick={switchModal}>
-              
-                <img
-                  className={styles.logo}
-                  src={auth.user.avatar}
-                  alt={auth.user.avatar}
-                  onClick={switchModal}
-                />
-              </div>
-              <div className={styles.divuser}>
-                <div className={styles.userhelp} id="userhelp">
-                  <span className={styles.username}>{auth.user.name} <span className={styles.underline}></span></span>
+            <div className={styles.divbottom}>
+              <div className={styles.flexdiv2}>
+                <div className={styles.singoutuser}>
+                  <Link href="/">
+                    <button className={styles.button1} onClick={logout}>
+                      <a className={styles.link6 + isActive("/")}>
+                        <FaSignOutAlt className={styles.icon6} />
+                        Sign out
+                      </a>
+                    </button>
+                  </Link>
+                </div>
+                <div className={styles.help1}>
+                  <button className={styles.dropbtn}>Profile</button>
+                  <div className={styles.dropdowncontent}>
+                    <div className={styles.logouser} onClick={switchModal}>
+                      <img
+                        className={styles.logo}
+                        src={auth.user.avatar}
+                        alt={auth.user.avatar}
+                        onClick={switchModal}
+                      />
+                    </div>
+                    <div className={styles.divuser}>
+                      <div className={styles.userhelp} id="userhelp">
+                        <span className={styles.username}>
+                          {auth.user.name}{" "}
+                          <span className={styles.underline}></span>
+                        </span>
 
-                  {/* <div className={styles.userarrow}>
-                &gt;
-              </div> */}
+                     
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
@@ -150,19 +153,18 @@ export default function NavigationLeftP() {
   };
   return (
     <Fragment>
-      {
-        Object.keys(auth).length === 0
-        ?
+      {Object.keys(auth).length === 0 ? (
         <div></div>
-/*           <div>
+      ) : (
+        /*           <div>
             <h1>Zugriff nicht erlaubt!</h1>
             <h2 style={{ color: 'black' }}>Melden Sie sich an</h2>
             <Link href="/login">
               <button>Anmelden</button>
             </Link>
           </div> */
-          : loggedRouter()
-      }
+        loggedRouter()
+      )}
     </Fragment>
   );
 }
